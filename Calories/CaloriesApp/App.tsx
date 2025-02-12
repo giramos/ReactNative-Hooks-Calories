79c89457d60d85f47bed6ac40088760859446289
giramos <UO202549@uniovi.es>
2025-02-12 13:32:43 +0100

    :heart: acabada practica :heart:

--- Calories/CaloriesApp/App.tsx
+++ Calories/CaloriesApp/App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Routes from "./src/routes/Routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Germán</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Routes />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- Calories/CaloriesApp/instalar_dependencias.bat
+++ Calories/CaloriesApp/instalar_dependencias.bat
@@ -1,15 +1,21 @@
 @echo off
-
 echo Instalando dependencias...
 
 echo Instalando React Navigation...
-npm install @react-navigation/native @react-navigation/stack
+call npm install @react-navigation/native @react-navigation/stack || exit /b
 
-echo Instalando React Native Elements...
-npm install react-native-elements
+echo Instalando React Native Elements (RNE UI)...
+call npm install @rneui/themed @rneui/base || exit /b
 
 echo Instalando React Native Vector Icons...
-npm install react-native-vector-icons
+call npm install react-native-vector-icons || exit /b
+
+echo Instalando AsyncStorage...
+call npm install @react-native-async-storage/async-storage || exit /b
+
+echo Instalando Date-fns...
+call npm install date-fns || exit /b
+call npm install --save-dev @types/date-fns || exit /b
 
 echo Instalación completa!
 pause
