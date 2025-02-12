4249a4c786cb112db5035673541ccbb0a3bb273c
giramos <UO202549@uniovi.es>
2025-02-12 12:01:59 +0100

    Install navigator and react Elements

---
+++ Calories/CaloriesApp/instalar_dependencias.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo Instalando dependencias...
+
+echo Instalando React Navigation...
+npm install @react-navigation/native @react-navigation/stack
+
+echo Instalando React Native Elements...
+npm install react-native-elements
+
+echo Instalando React Native Vector Icons...
+npm install react-native-vector-icons
+
+echo Instalación completa!
+pause
